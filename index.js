0fc4ba6f60a652ac0bd78d484d8f2229b5ce461c
KarenOrduna <kv8orduna@gmail.com>
2020-11-24 12:37:46 +0100

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 
 const comments = {
   id: 1,
