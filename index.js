177de55fcc9b0113a707f95ff6ed1f43953e9aa3
KarenOrduna <kv8orduna@gmail.com>
2020-11-24 11:27:38 +0100

    added lock and modified index

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hi');
+const express = require('express');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+const comments = {
+  id: 1,
+  name: 'John',
+  email: 'john@gmail.com',
+  message: 'great',
+};
+
+app.get('/comments', (request, response) => {
+  return response.json(comments);
+});
+
+app.listen(port, () => console.log(`Back end is listening on port ${port}`));
