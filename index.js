bf51e2582a0eee253c6f4541f4ad6fdd7157429d
KarenOrduna <kv8orduna@gmail.com>
2020-11-25 14:09:01 +0100

    finally Post works

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const uniqid = require ('uniqid');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-const comments = {
-  id: 1,
+const comments = [{ 
+  id: 1,  
   name: 'John',
   email: 'john@gmail.com',
   message: 'great',
-};
+}];
 
 app.get('/comments', (request, response) => {
   return response.json(comments);
+});
+
+app.post('/comments', (request,response) => {
+  const { message, name, email } = request.body;
+
+  const newComments = { message, name, email, id: uniqid() };
+  comments.push(newComments);
+
+response.json(newComments);
 });
 
 app.listen(port, () => console.log(`Back end is listening on port ${port}`));
